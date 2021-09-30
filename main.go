a39809d5eac6fbb4659a4125473932a3459d0e0b
patriciatozi <ptrc.tozi@hotmail.com>
2021-09-29 23:14:09 -0300

    feat: include jdkato library

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	prose "github.com/jdkato/prose/v2"
+)
+
+func main() {
+	// Create a new document with the default configuration:
+	doc, err := prose.NewDocument("Go is an open-source programming language created at Google.")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Iterate over the doc's tokens:
+	for _, tok := range doc.Tokens() {
+		fmt.Println(tok.Text, tok.Tag, tok.Label)
+	}
+
+	// Iterate over the doc's named-entities:
+	for _, ent := range doc.Entities() {
+		fmt.Println(ent.Text, ent.Label)
+		// Go GPE
+		// Google GPE
+	}
+
+	// Iterate over the doc's sentences:
+	for _, sent := range doc.Sentences() {
+		fmt.Println(sent.Text)
+		// Go is an open-source programming language created at Google.
+	}
+}
